b8cdcb04877d21daf6d8da487dc35341dcefd550
Deniz Dogan <denizdogan1101@gmail.com>
2025-08-23 13:16:45 +0100

    commit backend changes, routing + modify layout for favicon

--- table-code/next.config.ts
+++ table-code/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/favicon.ico",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
